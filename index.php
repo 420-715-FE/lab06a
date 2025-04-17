b3773b0ef0ed0975fd5af55aa53342d2ea396026
Pier-Luc Brault <pier-luc@brault.me>
2025-04-17 16:42:21 -0400

    Afficher les tâches

--- index.php
+++ index.php
@@ -16,8 +16,33 @@ $requeteTaches = $bd->query("
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php title($bd, $contact); ?></title>
+    <title>Liste de tâches</title>
     <link rel="stylesheet" href="water.css">
 </head>
-<body><?php body($bd, $contact) ?></body>
+<body>
+    <h1>Liste de tâches</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Complétée</th>
+                <th>Description</th>
+                <th>Priorité</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+
+            foreach ($requeteTaches as $tache) {
+                $checked = $tache['completee'] ? 'checked' : '';
+                echo "<tr>";
+                echo "<td><input type='checkbox' $checked></td>";
+                echo "<td>{$tache['description']}</td>";
+                echo "<td>{$tache['description_priorite']}</td>";
+                echo "</tr>";
+            }
+
+            ?>
+        </tbody>
+    </table>
+</body>
 </html>
